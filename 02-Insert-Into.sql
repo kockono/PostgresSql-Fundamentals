ad4ec58f460e91bb9ef53fd2c193007a587ce4df
kockono <kockonoop@gmail.com>
2024-04-09 18:20:29 -0600

    :sparkles: feat(Insert): Example

---
+++ 02-Insert-Into.sql
@@ -0,0 +1,8 @@
+-- Nombres de las columnas a insertar
+-- Seguido de los valores a insertar
+insert into nombre_de_la_tabla (nombre, email, edad) 
+  values ('nombre', 'email', 18);
+
+-- Si requieres insertar en todas las columnas, insertar en el mismo orden
+insert into nombre_de_la_tabla
+  values ('nombre', 'email', 18);
